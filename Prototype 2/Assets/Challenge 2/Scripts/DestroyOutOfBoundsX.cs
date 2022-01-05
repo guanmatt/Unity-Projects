809c8c835fbb94205937b12ee6c091233ba89e5a
guanmatt <m8guan@uwaterloo.ca>
2022-01-05 13:47:07 -0700

    finished challenge 2
    - used mutex to lock dog spawning until previous is removed
    *bit overkill

---
+++ Prototype 2/Assets/Challenge 2/Scripts/DestroyOutOfBoundsX.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.Threading;
+using UnityEngine;
+public static class MyLocks
+{
+    public static Mutex mut = new Mutex();
+    public static bool canSpawn = true;
+}
+public class DestroyOutOfBoundsX : MonoBehaviour
+{
+    private float leftLimit = -30;
+    private float bottomLimit = -5;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Destroy dogs if x position less than left limit
+        if (transform.position.x < leftLimit)
+        {
+            MyLocks.mut.WaitOne();
+            Destroy(gameObject);
+            MyLocks.canSpawn = true;
+            MyLocks.mut.ReleaseMutex();
+        } 
+        // Destroy balls if y position is less than bottomLimit
+        if (transform.position.y < bottomLimit)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
---
+++ Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if(Monitor.TryEnter(MyLocks.mut))
+            {
+                if(MyLocks.canSpawn == true)
+                {
+                    Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+                    MyLocks.canSpawn = false;
+                }
+                MyLocks.mut.ReleaseMutex();
+            }
+        }
+    }
+}
